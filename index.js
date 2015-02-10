d781afac7dc5908e0f86706ee0072a0d4564bd06
Christoph Witzko <github@christophwitzko.com>
2015-02-10 14:29:50 +0100

    feat: added configurable seperators back, added dump function
    BREAKING CHANGE: module exports now a factory

--- index.js
+++ index.js
@@ -2,44 +2,62 @@
 
 var assign = require('lodash.assign')
 
-/* eslint consistent-this: 0 */
-var PathObject = module.exports = function (obj) {
-  if (!(this instanceof PathObject)) return new PathObject(obj)
-  assign(this, obj)
-}
-
-PathObject.prototype.normalizePath = function (path) {
-  if (!path || typeof path !== 'string') return null
-  if (path[path.length - 1] === '/') {
-    path = path.substr(0, path.length - 1)
+module.exports = function (seperator) {
+  seperator = seperator || '/'
+  /* eslint consistent-this: 0 */
+  var PathObject = function (obj) {
+    if (!(this instanceof PathObject)) return new PathObject(obj)
+    assign(this, obj)
   }
-  if (path[0] === '/') {
-    path = path.substr(1)
-  }
-  return path.length ? path.split('/') : null
-}
 
-PathObject.prototype.get = function (path) {
-  var spath = this.normalizePath(path)
-  if (!spath) return null
-  var last = this
-  var err = spath.some(function (n, i) {
-    if (!n.length) return false
-    if (typeof last[n] === 'undefined') return true
-    last = last[n]
-  })
-  return err ? null : last
-}
-
-PathObject.prototype.set = function (path, value) {
-  var spath = this.normalizePath(path)
-  if (!spath) return null
-  var last = this
-  spath.forEach(function (n, i) {
-    if (!n.length) return
-    if (typeof last === 'object') {
-      last[n] = (spath.length - i > 1) ? (last[n] || {}) : value
+  PathObject.prototype.normalizePath = function (path) {
+    if (!path || typeof path !== 'string') return null
+    if (path[path.length - 1] === seperator) {
+      path = path.substr(0, path.length - 1)
     }
-    last = last[n]
-  })
+    if (path[0] === seperator) {
+      path = path.substr(1)
+    }
+    return path.length ? path.split(seperator) : null
+  }
+
+  PathObject.prototype.get = function (path) {
+    var spath = this.normalizePath(path)
+    if (!spath) return null
+    var last = this
+    var err = spath.some(function (n, i) {
+      if (!n.length) return false
+      if (typeof last[n] === 'undefined') return true
+      last = last[n]
+    })
+    return err ? null : last
+  }
+
+  PathObject.prototype.set = function (path, value) {
+    var spath = this.normalizePath(path)
+    if (!spath) return null
+    var last = this
+    spath.forEach(function (n, i) {
+      if (!n.length) return
+      if (typeof last === 'object') {
+        last[n] = (spath.length - i > 1) ? (last[n] || {}) : value
+      }
+      last = last[n]
+    })
+  }
+
+  function traverse (retObj, root, obj) {
+    Object.keys(obj).forEach(function (k) {
+      if (typeof obj[k] === 'object') return traverse(retObj, root + k + seperator, obj[k])
+      retObj[root + k] = obj[k]
+    })
+  }
+
+  PathObject.prototype.dump = function () {
+    var retObj = {}
+    traverse(retObj, '', this)
+    return retObj
+  }
+
+  return PathObject
 }
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 var test = require('tape')
 
-var PathObject = require('./')
+var PathObject = require('./')()
 
 test('set path', function (t) {
   t.plan(3)
@@ -34,4 +34,23 @@ test('get path', function (t) {
   t.equal(po.get('my/cool'), po.my.cool, 'get 1')
   t.equal(po.get('/cd/pwd/xd/'), po.cd.pwd.xd, 'get 2')
   t.equal(po.get('cd/pwd/xd/master'), po.cd.pwd.xd.master, 'get 3')
+})
+
+test('dump paths', function (t) {
+  t.plan(3)
+  var po = new PathObject({
+    my: {
+      cool: 'path'
+    },
+    cd: {
+      pwd: {
+        master: 'mx'
+      }
+    },
+    HEAD: '123123'
+  })
+  var dump = po.dump()
+  t.equal(dump['my/cool'], po.my.cool, 'dump 1')
+  t.equal(dump['cd/pwd/master'], po.cd.pwd.master, 'dump 2')
+  t.equal(dump['HEAD'], po.HEAD, 'dump 3')
 })
