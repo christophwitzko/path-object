e78e24c6a3d82d597055a16ea4d71a8b2a2e6a8e
Christoph Witzko <github@christophwitzko.com>
2015-02-20 19:22:08 +0100

    fix: do not traverse buffers

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
 'use strict'
 
 var assign = require('lodash.assign')
+var cutil = require('core-util-is')
 
 module.exports = function (seperator) {
   seperator = seperator || '/'
+
+  function isValue (val) {
+    return cutil.isBuffer(val) ||
+            cutil.isDate(val) ||
+            cutil.isError(val) ||
+            cutil.isFunction(val) ||
+            cutil.isRegExp(val) ||
+            cutil.isPrimitive(val)
+  }
+
+  function isObject (val) {
+    return cutil.isObject(val) && !isValue(val)
+  }
+
   /* eslint consistent-this:0 */
   var PathObject = function (obj) {
     if (!(this instanceof PathObject)) return new PathObject(obj)
-    assign(this, obj)
+    if (isObject(obj)) assign(this, obj)
   }
 
-  PathObject.prototype.normalizePath = function (path) {
-    if (!path || typeof path !== 'string') return null
+  function normalizePath (path) {
+    if (!path || !cutil.isString(path)) return null
     if (path[path.length - 1] === seperator) {
       path = path.substr(0, path.length - 1)
     }
     if (path[0] === seperator) {
       path = path.substr(1)
     }
-    return path.length ? path.split(seperator) : null
+    if (!path.length) return null
+    var spath = path.split(seperator)
+    spath = spath.filter(function (e) {
+      return e
+    })
+    return spath.length ? spath : null
   }
 
   PathObject.prototype.get = function (path) {
-    var spath = this.normalizePath(path)
+    var spath = normalizePath(path)
     if (!spath) return null
     var last = this
     var err = spath.some(function (n, i) {
-      if (typeof last[n] === 'undefined') return true
+      if (cutil.isUndefined(last[n])) return true
       last = last[n]
     })
     return err ? null : last
   }
 
   PathObject.prototype.set = function (path, value) {
-    var spath = this.normalizePath(path)
+    var spath = normalizePath(path)
     if (!spath) return null
     var last = this
     spath.forEach(function (n, i) {
-      if (typeof last === 'object' && last) {
-        last[n] = (spath.length - i > 1) ? ((typeof last[n] === 'object' && last[n]) ? last[n] : {}) : value
+      if (isObject(last)) {
+        last[n] = (spath.length - i > 1) ?
+                      (isObject(last[n]) ? last[n] : {}) :
+                      value
       }
       last = last[n]
     })
@@ -46,8 +68,11 @@ module.exports = function (seperator) {
 
   function traverse (retObj, root, obj) {
     Object.keys(obj).forEach(function (k) {
-      if (typeof obj[k] === 'object' && obj[k]) return traverse(retObj, root + k + seperator, obj[k])
-      retObj[root + k] = obj[k]
+      if (isValue(obj[k])) {
+        retObj[root + k] = obj[k]
+        return
+      }
+      traverse(retObj, root + k + seperator, obj[k])
     })
   }
 
@@ -57,12 +82,12 @@ module.exports = function (seperator) {
     var startObj = this
     if (root) {
       var srobj = this.get(root)
-      if (typeof srobj === 'object' && srobj) {
-        nroot = this.normalizePath(root).join(seperator) + seperator
+      if (isObject(srobj)) {
+        nroot = strip ? '' : normalizePath(root).join(seperator) + seperator
         startObj = srobj
       } else return {}
     }
-    traverse(retObj, strip ? '' : nroot, startObj)
+    traverse(retObj, nroot, startObj)
     return retObj
   }
 
--- test.js
+++ test.js
@@ -19,7 +19,7 @@ test('set path', function (t) {
 })
 
 test('get path', function (t) {
-  t.plan(4)
+  t.plan(5)
   /* eslint new-cap: 0 */
   var po = PathObject({
     my: {
@@ -35,12 +35,13 @@ test('get path', function (t) {
   })
   t.equal(po.get('my/cool'), po.my.cool, 'get 1')
   t.equal(po.get('/cd/pwd/xd/'), po.cd.pwd.xd, 'get 2')
-  t.equal(po.get('cd/pwd/xd/master'), po.cd.pwd.xd.master, 'get 3')
-  t.equal(po.get('foo/bar'), null, 'get 4')
+  t.equal(po.get('/cd//pwd///xd//master/'), po.cd.pwd.xd.master, 'get 3')
+  t.equal(po.get('cd/pwd/xd/master'), po.cd.pwd.xd.master, 'get 4')
+  t.equal(po.get('foo/bar'), null, 'get 5')
 })
 
 test('dump paths', function (t) {
-  t.plan(7)
+  t.plan(8)
   var po = new PathObject({
     my: {
       cool: 'path',
@@ -50,20 +51,25 @@ test('dump paths', function (t) {
     },
     cd: {
       pwd: {
-        master: 'mx'
+        master: 'mx',
+        exp: /^self/,
+        timestamp: new Date()
       }
     },
+    zero: null,
+    buf: new Buffer('test'),
     HEAD: '123123'
   })
   var dump = po.dump()
   t.equal(dump['my/cool'], po.my.cool, 'dump 1')
   t.equal(dump['cd/pwd/master'], po.cd.pwd.master, 'dump 2')
   t.equal(dump['HEAD'], po.HEAD, 'dump 3')
+  t.equal(dump['buf'].toString(), po.buf.toString(), 'dump 4')
   var nspDump1 = po.dump('my')
-  t.equal(nspDump1['my/cool'], po.my.cool, 'dump 4')
-  t.equal(nspDump1['my/nice/way'], po.my.nice.way, 'dump 5')
+  t.equal(nspDump1['my/cool'], po.my.cool, 'dump 5')
+  t.equal(nspDump1['my/nice/way'], po.my.nice.way, 'dump 6')
   var nspDump2 = po.dump('cd', true)
-  t.equal(nspDump2['pwd/master'], po.cd.pwd.master, 'dump 6')
+  t.equal(nspDump2['pwd/master'], po.cd.pwd.master, 'dump 7')
   var nspDump3 = po.dump('asd')
-  t.deepEqual(nspDump3, {}, 'dump 7')
+  t.deepEqual(nspDump3, {}, 'dump 8')
 })
